9a1e7036150afd4db825f554dcee33152af24d11
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-07-17 22:53:30 +0530

    views

--- FullStack/Django/firstApp/views.py
+++ FullStack/Django/firstApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from firstApp.models import Topic,WebPage,AccessRecord
 
 def index(request):
-    return HttpResponse("Hello world")
+    webpageList = AccessRecord.objects.order_by('date')
+    dateDict = {'access_records':webpageList}
+    # return HttpResponse("Hello world")
+    #myDict = {'insert_me':"Hello I'm from views.py from firstApp"}
+    return render(request,'firstApp/index.html',context=dateDict)
